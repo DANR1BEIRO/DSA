ed6418397cc57f1eab49dc70a4c18fc5a8bcbed2
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-12 13:31:27 -0300

    Implement a Frequency Map to determine if two words are anagrams

---
+++ src/Array/FrequencyMap.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class FrequencyMap {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] countChar = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            countChar[s.charAt(i) - 'a']++;
+            countChar[t.charAt(i) - 'a']--;
+        }
+        for (int count : countChar) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("daniel", "leinad"));
+    }
+}
